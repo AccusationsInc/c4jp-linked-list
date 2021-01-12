1d020df0108a322819a3885d4a1c98be84902cff
accusationsinc <nathan@LAPTOP-HM2S364V.localdomain>
2021-01-11 21:01:12 -0600

    This is the first commit made to the linked list assignment. Implemented most changes.

--- linked_list.c
+++ linked_list.c
@@ -8,79 +8,112 @@ void ll_init(struct ll_node *node) {
 
 bool ll_has_next(struct ll_node *node) {
     assert(node != NULL);
-    // TODO: Implement your solution here.
-    return false;
+    if(!node->next) 
+        return false;
+    return true;
 }
 
 bool ll_has_prev(struct ll_node *node) {
     assert(node != NULL);
-    // TODO: Implement your solution here.
-    return false;
+    if(!node->prev)
+        return false;
+    return true;
 }
 
 struct ll_node *ll_next(struct ll_node *node) {
     assert(node != NULL);
-    // TODO: Implement your solution here.
-    return NULL;
+    return node->next;
 }
 
 struct ll_node *ll_prev(struct ll_node *node) {
     assert(node != NULL);
-    // TODO: Implement your solution here.
-    return NULL;
+    return node->prev;
 }
 
 size_t ll_size(struct ll_node *head) {
     assert(head != NULL);
-    // TODO: Implement your solution here.
+    unsigned long size = 0;
+    struct ll_node *temp = head;
+    while(ll_has_next(head)) {
+        size++;
+        temp = temp->next;
+    }
     return 0;
 }
 
 struct ll_node *ll_head(struct ll_node *list) {
     assert(list != NULL);
-    // TODO: Implement your solution here.
-    return NULL;
+    struct ll_node *temp = list;
+    while(ll_has_prev(list)){
+        temp = temp->prev
+    }
+    return temp;
 }
 
 struct ll_node *ll_tail(struct ll_node *list) {
     assert(list != NULL);
-    // TODO: Implement your solution here.
-    return NULL;
+    struct ll_node *temp = list;
+    while(ll_has_next(list)) {
+        temp = temp->next;
+    }
+    return temp;
 }
 
 struct ll_node *ll_get(struct ll_node *node, size_t index) {
     assert(node != NULL);
-    // TODO: Implement your solution here.
-    return NULL;
+    if(ll_size(node) > index) {
+        struct ll_node *temp = node;
+        for(int i = 0; i < index; i++) {
+            temp = temp->next;
+        }
+        return temp;
+    } else {
+        printf("Error :: index cannot be less than size");
+        exit(0);
+    }
 }
 
 void ll_insert_before(struct ll_node *new, struct ll_node *existing) {
     assert(new != NULL);
     assert(existing != NULL);
-    // TODO: Implement your solution here.
+    new->next = existing;
+    new->prev = existing->prev;
+    existing->prev = new;
+    new->prev->next = new;
 }
 
 void ll_insert_after(struct ll_node *new, struct ll_node *existing) {
     assert(new != NULL);
     assert(existing != NULL);
-    // TODO: Implement your solution here.
+    new->prev = existing;
+    new->next = existing->next;
+    existing->next = new;
+    new->next->prev = new;
 }
 
 void ll_insert_first(struct ll_node *new, struct ll_node *list) {
     assert(new != NULL);
     assert(list != NULL);
-    // TODO: Implement your solution here.
+    struct ll_node *head = ll_head(list);
+    new->next = head;
+    head->prev = new;
 }
 
 void ll_insert_last(struct ll_node *new, struct ll_node *list) {
     assert(new != NULL);
     assert(list != NULL);
-    // TODO: Implement your solution here.
+    struct ll_node *tail = ll_tail;
+    new->prev = tail;
+    tail->next = new;
 }
 
 void ll_remove(struct ll_node *node) {
     assert(node != NULL);
-    // TODO: Implement your solution here.
+    if(node->prev) 
+        node->prev->next = node->next;
+    if(node->next)
+        node->next->prev = node->prev;
+    free(node);
 }
 
 struct ll_node *ll_min(struct ll_node *list, ll_comparator_t comparator) {
